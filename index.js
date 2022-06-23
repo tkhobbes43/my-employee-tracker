e84cdc6b8488de34923608602ebb01ed1979fed0
Tommy Lee <t.k.hobbes43@gmail.com>
2022-06-22 23:35:12 -0500

    got inquirer to list user with all the choicese, but Im going to have to psuedo code more and see what code will say now for every response

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const inquirer = require('inquirer');
 const cTable = require('console.table');
 
 // connecting to database
-const connection = mysql.createConnection([
+const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root'
-    password: ''
+    user: 'root',
+    password: '',
     database: 'employee_db'
-]);
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    afterConnection();
+});
 
 // function that shows welcome image after connection
 afterConnection = () => {
@@ -21,3 +27,38 @@ afterConnection = () => {
     console.log("`-------------------------------------------------'")
     promptUser();
 };
+
+// inquirer prompt for what user wants to do
+const promptUser = () => {
+    inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices: ['View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add an employee',
+                    'Update an employee',
+                    'Update employee managers',
+                    'View employees by manager',
+                    'View employees by department',
+                    'Delete a department',
+                    'Delete a role',
+                    'Delete an employee',
+                    'View the total utilized budget of a department']
+        }
+    ])
+    .then((answers) => {
+
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+          console.log("Prompt couldn't be rendered in the current environment")
+        } else {
+            console.log("Something else went wrong")
+        }
+    });
+}
