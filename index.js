ebbd8bf6916ae84c5b66d03ae0d7aa5c0d224a25
Tommy Lee <t.k.hobbes43@gmail.com>
2022-06-22 23:05:59 -0500

    working on connection to database with mysql2, but not complete, need to have error part of code still entered

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+// all the npm packages that need to require
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+
+// connecting to database
+const connection = mysql.createConnection([
+    host: 'localhost',
+    user: 'root'
+    password: ''
+    database: 'employee_db'
+]);
+
+// function that shows welcome image after connection
+afterConnection = () => {
+    console.log(",-------------------------------------------------.")
+    console.log("|                                                 |")
+    console.log("|                  My Employee                    |")
+    console.log("|                    Tracker                      |")
+    console.log("|                                                 |")
+    console.log("`-------------------------------------------------'")
+    promptUser();
+};
