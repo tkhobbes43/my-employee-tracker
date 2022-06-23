90d9c811582e3ef0d5779d60a3c6ef36230defd2
Tommy Lee <t.k.hobbes43@gmail.com>
2022-06-22 22:37:04 -0500

    finished entering data for seeds

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (name)
+VALUES ("Engineering"),
+    ("Finance & Accounting"),
+    ("Sales & Marketing"),
+    ("Operations");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Engineer", 90000, 1),
+    ("Project Manager", 120000, 1),
+    ("Accountant", 145000, 2), 
+    ("Finanical Analyst", 150000, 2),
+    ("Marketing Coordindator", 80000, 3), 
+    ("Sales Lead", 90000, 3),
+    ("Operations Manager", 100000, 4),
+    ("Operations Supervisor", 90000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Abed", "Nadir", 2, null)
+    ("Britta", "Perry", 1, 2)
+    ("Jeff", "Winger", 4, null)
+    ("Troy", "Barnes", 3, 7)
+    ("Annie", "Edison", 6, null)
+    ("Pierce", "Hawthorne", 5, null)
+    ("Shirley", "Bennett", 7, null)
+    ("Ben", "Chang", 8, null) 
